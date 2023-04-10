0bda97e63037f03f75b1eb4620022bdd4be8493b
trainingdbt1989@gmail.com <trainingdbt1989@gmail.com>
2023-04-10 16:55:26 +0000

    commit

--- models/Training/store.sql
+++ models/Training/store.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 with store_tbl_2023 as
 (
-select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY from PROD.Store_2023
+select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY from Prod.Store_2023
 )
 select * from store_tbl_2023
